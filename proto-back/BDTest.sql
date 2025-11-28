d50c928aba8d28c0e974894fb0d4ac5022437d77
Junior FOKOU <juniorfokou995@yahoo.fr>
2025-11-28 16:03:45 +0000

    Initial commit: backend + frontend

---
+++ proto-back/BDTest.sql
@@ -0,0 +1,20 @@
+
+CREATE DATABASE IF NOT EXISTS BDTest;
+USE BDTest;
+
+
+CREATE TABLE IF NOT EXISTS sample (
+                                      id_sample INT AUTO_INCREMENT PRIMARY KEY,
+                                      date_sample DATE NOT NULL,
+                                      string_sample VARCHAR(50),
+                                      float_sample FLOAT,
+                                      sample_type ENUM('SAMPLE_TYPE1', 'SAMPLE_TYPE2')
+);
+
+
+INSERT INTO sample (date_sample, string_sample, float_sample, sample_type)
+VALUES
+    ('2017-03-14', '3', 2, 'SAMPLE_TYPE1'),
+    ('2018-03-14', '2', 3, 'SAMPLE_TYPE2'),
+    ('2017-03-14', '1', 4, 'SAMPLE_TYPE1'),
+    ('2019-03-14', '4', 1, 'SAMPLE_TYPE2');
