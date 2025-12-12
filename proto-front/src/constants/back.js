923e773824446f7d3c4f05f20230f3b33332d755
Junior Fokou <juniorfokou995@yahoo.fr>
2025-12-12 19:02:58 +0100

    test ajout sample

--- proto-front/src/constants/back.js
+++ proto-front/src/constants/back.js
@@ -1,5 +1,5 @@
 
-export const LOCAL_HOST = 'http://localhost:8080';
-export const LOCAL_HOST_SAMPLE = LOCAL_HOST + '/api/samples/';
+export const LOCAL_HOST = 'http://172.31.253.230:8080';
+export const LOCAL_HOST_SAMPLE = LOCAL_HOST + '/samples/';
 export const GET_SAMPLES = LOCAL_HOST_SAMPLE + 'all';
 export const UPDATE_SAMPLES = LOCAL_HOST_SAMPLE + 'update';
