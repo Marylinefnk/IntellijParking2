7e6e2ee1b8f38a29af501fbcfed513a996b22891
Junior Fokou <juniorfokou995@yahoo.fr>
2025-12-18 12:47:02 +0100

    test

--- proto-front/src/constants/back.js
+++ proto-front/src/constants/back.js
@@ -1,5 +1,5 @@
 
-export const LOCAL_HOST = 'http://172.31.253.230:8080';
-export const LOCAL_HOST_SAMPLE = LOCAL_HOST + '/samples/';
+export const LOCAL_HOST = 'http://172.31.252.229:8080';
+export const LOCAL_HOST_SAMPLE = LOCAL_HOST + '/sample/';
 export const GET_SAMPLES = LOCAL_HOST_SAMPLE + 'all';
 export const UPDATE_SAMPLES = LOCAL_HOST_SAMPLE + 'update';
