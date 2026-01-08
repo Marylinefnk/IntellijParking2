c322cb4305fd27fb8d152af1e046ad2d65af564b
Maryline <marylinefonkou@gmail.com>
2026-01-08 02:17:36 +0100

    Ajout des entités JPA (Allee, Personne, Place, Intersection, Zone, Vehicule, Reservation)

---
+++ proto-back/src/main/java/esiag/back/models/sample/InteractionType.java
@@ -0,0 +1,6 @@
+package esiag.back.models.sample;
+
+public enum InteractionType {
+    ENTREE, SORTIE, VIRAGE, CROISEMENT;
+
+}
---
+++ proto-back/src/main/java/esiag/back/models/sample/ReservationStatut.java
@@ -0,0 +1,10 @@
+package esiag.back.models.sample;
+
+public enum ReservationStatut {
+    //CONFIRMEE, EN_COURS, TERMINEE, ANNULEE;
+    confirmee,
+    en_attente,
+    annulee,
+    terminee,
+    en_cours;
+}
