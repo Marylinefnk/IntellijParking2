f9d5f1a84a27048ce4d8405f96534ff611446a05
Tatiana-Ndoumbeu <tatianandoumbeu@yahoo.com>
2026-02-13 09:30:00 +0100

    SCRUM-7 service init complet

--- intellijParking-backend/src/main/java/intellijP/back/services/SimulationService.java
+++ intellijParking-backend/src/main/java/intellijP/back/services/SimulationService.java
@@ -9,6 +9,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Random;
 
 @Service
@@ -45,8 +46,76 @@ public class SimulationService {
 
     @Transactional
     public InitialisationResultatDTO initialiser(int nbPersonnes, int maxVehiculesParPersonne, Long seed) {
-        logger.info("Initialisation simulation - {} personnes", nbPersonnes);
-        return new InitialisationResultatDTO(0, 0, 0);
+        logger.info("Initialisation simulation - {} personnes, max {} vehicules/personne", nbPersonnes, maxVehiculesParPersonne);
+
+        Random rng = seed != null ? new Random(seed) : new Random();
+        int compteurPersonnes = 0;
+        int compteurVehicules = 0;
+
+        String[] prenoms = {"Alice", "Bob", "Charlie", "Diana", "Eric", "Fatou", "Georges", "Hana",
+                "Ibrahim", "Julie", "Kevin", "Laura", "Marc", "Nina", "Oscar", "Paula"};
+        String[] noms = {"Martin", "Bernard", "Thomas", "Petit", "Robert", "Durand", "Leroy", "Moreau",
+                "Simon", "Laurent", "Lefebvre", "Michel", "Garcia", "David", "Bertrand", "Roux"};
+
+        for (int i = 0; i < nbPersonnes; i++) {
+            String prenom = prenoms[rng.nextInt(prenoms.length)];
+            String nom = noms[rng.nextInt(noms.length)];
+            String mail = prenom.toLowerCase() + "." + nom.toLowerCase() + i + "@parking-sirius.fr";
+
+            Personne p = Personne.builder()
+                    .prenom(prenom)
+                    .nom(nom)
+                    .mail(mail)
+                    .password(passwordEncoder.encode("sirius2026"))
+                    .typePersonne(TypePersonne.ABONNE)
+                    .build();
+            personneRepository.save(p);
+            compteurPersonnes++;
+
+            int nbVehicules = 1 + rng.nextInt(maxVehiculesParPersonne);
+            TypeVehicule[] types = TypeVehicule.values();
+
+            for (int j = 0; j < nbVehicules; j++) {
+                String immat = genererImmatriculation(rng);
+                if (vehiculeRepository.findByImmatriculation(immat).isPresent()) {
+                    immat = immat + "-" + rng.nextInt(99);
+                }
+                Vehicule v = Vehicule.builder()
+                        .immatriculation(immat)
+                        .typeVehicule(types[rng.nextInt(types.length)])
+                        .personne(p)
+                        .build();
+                vehiculeRepository.save(v);
+                compteurVehicules++;
+            }
+        }
+
+        List<Place> toutesLesPlaces = placeRepository.findAll();
+        int compteurCapteurs = 0;
+
+        for (Place place : toutesLesPlaces) {
+            if (!capteurRepository.existsByPlaceId(place.getId())) {
+                Capteur c = Capteur.builder()
+                        .place(place)
+                        .etatCapteur(EtatCapteur.ACTIF)
+                        .presenceDetectee(false)
+                        .build();
+                capteurRepository.save(c);
+                compteurCapteurs++;
+            }
+        }
+
+        logger.info("Init terminée: {} personnes, {} véhicules, {} capteurs créés",
+                compteurPersonnes, compteurVehicules, compteurCapteurs);
+
+        return new InitialisationResultatDTO(compteurPersonnes, compteurVehicules, compteurCapteurs);
+    }
+
+    private String genererImmatriculation(Random rng) {
+        String lettres1 = String.valueOf((char) ('A' + rng.nextInt(26))) + (char) ('A' + rng.nextInt(26));
+        int chiffres = 100 + rng.nextInt(900);
+        String lettres2 = String.valueOf((char) ('A' + rng.nextInt(26))) + (char) ('A' + rng.nextInt(26));
+        return lettres1 + "-" + chiffres + "-" + lettres2;
     }
 
 }
