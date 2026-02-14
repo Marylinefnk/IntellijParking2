5cecb16485bef17184df4af084b24db0d708f249
Tatiana-Ndoumbeu <tatianandoumbeu@yahoo.com>
2026-02-14 20:27:00 +0100

    SCRUM-8 wip generation reservations squelette

--- intellijParking-backend/src/main/java/intellijP/back/services/SimulationService.java
+++ intellijParking-backend/src/main/java/intellijP/back/services/SimulationService.java
@@ -5,10 +5,12 @@ import intellijP.back.models.*;
 import intellijP.back.repositories.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Random;
 
@@ -47,68 +49,48 @@ public class SimulationService {
     @Transactional
     public InitialisationResultatDTO initialiser(int nbPersonnes, int maxVehiculesParPersonne, Long seed) {
         logger.info("Initialisation simulation - {} personnes, max {} vehicules/personne", nbPersonnes, maxVehiculesParPersonne);
-
         Random rng = seed != null ? new Random(seed) : new Random();
         int compteurPersonnes = 0;
         int compteurVehicules = 0;
-
         String[] prenoms = {"Alice", "Bob", "Charlie", "Diana", "Eric", "Fatou", "Georges", "Hana",
                 "Ibrahim", "Julie", "Kevin", "Laura", "Marc", "Nina", "Oscar", "Paula"};
         String[] noms = {"Martin", "Bernard", "Thomas", "Petit", "Robert", "Durand", "Leroy", "Moreau",
                 "Simon", "Laurent", "Lefebvre", "Michel", "Garcia", "David", "Bertrand", "Roux"};
-
         for (int i = 0; i < nbPersonnes; i++) {
             String prenom = prenoms[rng.nextInt(prenoms.length)];
             String nom = noms[rng.nextInt(noms.length)];
             String mail = prenom.toLowerCase() + "." + nom.toLowerCase() + i + "@parking-sirius.fr";
-
-            Personne p = Personne.builder()
-                    .prenom(prenom)
-                    .nom(nom)
-                    .mail(mail)
-                    .password(passwordEncoder.encode("sirius2026"))
-                    .typePersonne(TypePersonne.ABONNE)
-                    .build();
+            Personne p = Personne.builder().prenom(prenom).nom(nom).mail(mail)
+                    .password(passwordEncoder.encode("sirius2026")).typePersonne(TypePersonne.ABONNE).build();
             personneRepository.save(p);
             compteurPersonnes++;
-
             int nbVehicules = 1 + rng.nextInt(maxVehiculesParPersonne);
             TypeVehicule[] types = TypeVehicule.values();
-
             for (int j = 0; j < nbVehicules; j++) {
                 String immat = genererImmatriculation(rng);
-                if (vehiculeRepository.findByImmatriculation(immat).isPresent()) {
-                    immat = immat + "-" + rng.nextInt(99);
-                }
-                Vehicule v = Vehicule.builder()
-                        .immatriculation(immat)
-                        .typeVehicule(types[rng.nextInt(types.length)])
-                        .personne(p)
-                        .build();
-                vehiculeRepository.save(v);
+                if (vehiculeRepository.findByImmatriculation(immat).isPresent()) immat = immat + "-" + rng.nextInt(99);
+                vehiculeRepository.save(Vehicule.builder().immatriculation(immat)
+                        .typeVehicule(types[rng.nextInt(types.length)]).personne(p).build());
                 compteurVehicules++;
             }
         }
-
         List<Place> toutesLesPlaces = placeRepository.findAll();
         int compteurCapteurs = 0;
-
         for (Place place : toutesLesPlaces) {
             if (!capteurRepository.existsByPlaceId(place.getId())) {
-                Capteur c = Capteur.builder()
-                        .place(place)
-                        .etatCapteur(EtatCapteur.ACTIF)
-                        .presenceDetectee(false)
-                        .build();
-                capteurRepository.save(c);
+                capteurRepository.save(Capteur.builder().place(place).etatCapteur(EtatCapteur.ACTIF).presenceDetectee(false).build());
                 compteurCapteurs++;
             }
         }
-
-        logger.info("Init terminée: {} personnes, {} véhicules, {} capteurs créés",
-                compteurPersonnes, compteurVehicules, compteurCapteurs);
-
+        logger.info("Init terminée: {} personnes, {} véhicules, {} capteurs créés", compteurPersonnes, compteurVehicules, compteurCapteurs);
         return new InitialisationResultatDTO(compteurPersonnes, compteurVehicules, compteurCapteurs);
+    }
+
+    @Async
+    @Transactional
+    public void genererReservationsJournee(LocalDate date, int pasSecondes, String niveau, Double tauxCible) {
+        logger.info("Génération réservations journée {} - niveau={}", date, niveau);
+        // TODO: implement
     }
 
     private String genererImmatriculation(Random rng) {
