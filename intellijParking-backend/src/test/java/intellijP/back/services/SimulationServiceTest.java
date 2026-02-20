151e2f520c69991575dda9dd50aba61e0724cc91
Tatiana-Ndoumbeu <tatianandoumbeu@yahoo.com>
2026-02-20 17:28:00 +0100

    SCRUM-11 #time 3h ajout tests unitaires placeholder

---
+++ intellijParking-backend/src/test/java/intellijP/back/services/SimulationServiceTest.java
@@ -0,0 +1,14 @@
+package intellijP.back.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimulationServiceTest {
+
+    @Test
+    void placeholder() {
+        assertTrue(true);
+    }
+
+}
