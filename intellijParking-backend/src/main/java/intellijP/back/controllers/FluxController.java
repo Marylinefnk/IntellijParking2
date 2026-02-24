2a13fc00aa390479f9ee4bf43824a1dc408b05c0
Tatiana-Ndoumbeu <tatianandoumbeu@yahoo.com>
2026-02-24 14:10:00 +0100

    SCRUM-10 ajout logs connexion SSE

--- intellijParking-backend/src/main/java/intellijP/back/controllers/FluxController.java
+++ intellijParking-backend/src/main/java/intellijP/back/controllers/FluxController.java
@@ -1,6 +1,8 @@
 package intellijP.back.controllers;
 
 import intellijP.back.services.FluxSSEService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -9,16 +11,22 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 @RequestMapping("/flux")
 public class FluxController {
 
+    private static final Logger logger = LoggerFactory.getLogger(FluxController.class);
+
     private final FluxSSEService fluxSSEService;
 
     public FluxController(FluxSSEService fluxSSEService) {
         this.fluxSSEService = fluxSSEService;
     }
 
+    // endpoint SSE pour les changements d'état des places
+    // produces text/event-stream pour que EventSource côté React fonctionne
     @GetMapping(value = "/places", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public SseEmitter fluxPlaces(
             @RequestParam(required = false) String niveau,
             @RequestParam(required = false) Long idPlace) {
+
+        logger.info("Nouvelle connexion SSE /flux/places - niveau={}, idPlace={}", niveau, idPlace);
         return fluxSSEService.abonnerPlaces(niveau, idPlace);
     }
 
