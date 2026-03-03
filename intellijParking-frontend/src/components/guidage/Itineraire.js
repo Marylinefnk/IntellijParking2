43b8447823df9194cfd3920833b0a68a75468d2d
Maryline <marylinefonkou@gmail.com>
2026-03-03 23:25:12 +0100

    API itineraire

--- intellijParking-frontend/src/components/guidage/Itineraire.js
+++ intellijParking-frontend/src/components/guidage/Itineraire.js
@@ -1,11 +1,24 @@
 import React, { useEffect, useState } from 'react';
-import planParking from '../../assets/plan_parking.png';
 import ParkingPage from '../parking/ParkingPage';
 
-const ID_RESERVATION = 18;
+//const responseItineraire = await fetch('/api/guidage/itineraire/18'); //ici je fais pour la reservation 18
+//const responseItineraire = await fetch('/api/guidage/itineraire/${idreservation}'); //pour toute réservation envoyée par api
 
 export default function Itineraire() {
+    const [cheminData, setCheminData] = useState([])
+
+    useEffect(() => {
+        fetch("/api/guidage/itineraire/${idreservation}")
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setCheminData(data);
+        })
+    }, []);
+
+
     return (
+
         <section>
             <h1> Itinéraire vers votre place </h1>
             <ParkingPage/>
@@ -16,4 +29,5 @@ export default function Itineraire() {
 
 
     );
+
 }
