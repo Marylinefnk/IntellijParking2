bc9feca38921b7913fa2d542c6f17a7ba301b56b
Maryline <marylinefonkou@gmail.com>
2026-04-30 00:27:08 +0200

    Ajout authentification + récupération de la réservation active

--- intellijParking-frontend/src/components/guidage/Itineraire.js
+++ intellijParking-frontend/src/components/guidage/Itineraire.js
@@ -1,20 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import Niveau1 from '../parking/Niveau1';
 import { API_GUIDAGE} from '../../constants/back';
+import {API_RESERVATIONS_PERSONNE} from '../../constants/back';
+import {useUser} from '../../context/UserContext';
+
 
 
 
 export default function Itineraire() {
     const [cheminData, setCheminData] = useState([])
+    const {user, authFetch } = useUser()
 
     useEffect(() => {
-        fetch(`${API_GUIDAGE}/3687`)
-            .then((response) => response.json())
-            .then((data) => {
-                setCheminData(data);
-                console.log(data);
-            })
-    }, []);
+        fetchItineraire();
+    }, [user]);
+
+
+    const fetchItineraire = async () => {
+        const response = await authFetch(`${API_RESERVATIONS_PERSONNE}/${user.id}`)
+        const  reservations  = await response.json()
+        console.log(reservations);
+
+        const reservationsActive =
+            reservations.find(reservation => reservation.statut === 'EN_COURS')
+
+        if (!reservationsActive) return;
+
+        const itineraire = await authFetch(`${API_GUIDAGE}/${reservationsActive.id}`);
+        const data = await itineraire.json();
+        console.log(data);
+        setCheminData(data);
+
+    }
+
 
     const points = () => {
         let count = "";
@@ -24,7 +42,6 @@ export default function Itineraire() {
         } return count
     }
 
-
     return (
 
         <section>
@@ -33,7 +50,8 @@ export default function Itineraire() {
             <div style={{ position: 'relative', width: '1350px', height: '600px' }}>
                 <Niveau1/>
                 <svg
-                    style={{ position: 'absolute', width: '1350px', height: '600px', top: 0, left: 0, zIndex: 99 }}>
+                    style={{ position: 'absolute', width: '1350px', height: '600px', top: 0, left: 0, zIndex: 999 }}>
+
 
                     <polyline
                         points={points()}
